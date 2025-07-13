43faa036b716d5fc42180f1579161045b44e7130
Prahalad-ship-it <srinp131@gmail.com>
2025-07-12 17:44:50 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Leaderboard from "/Leaderboard.jsx";
+
+function App() {
+  return (
+    <div className="bg-orange-100 min-h-screen flex items-center justify-center">
+      <Leaderboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Leaderboard.jsx
@@ -0,0 +1,12 @@
+import Leaderboard from "./components/Leaderboard.jsx"; // also okay
+
+// src/components/Leaderboard.jsx
+
+export default function Leaderboard() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold text-red-600">🏆 Leaderboard</h1>
+      <p className="text-gray-500">Leaderboard will show here soon.</p>
+    </div>
+  );
+}
